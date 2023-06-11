beaafd7e3ff0bc44e4270641ff72a1743ad7a5ea
jsardello <jackson.sardello@gmail.com>
2023-06-10 21:17:42 -0500

    Setup further Wallet Stats callouts

--- addons/xrpl4gd/WalletStats.gd
+++ addons/xrpl4gd/WalletStats.gd
@@ -4,27 +4,79 @@ extends Node
 func _ready():
 	print("testing")
 
-func addressLookup():
-	print('Hello World')
+func addressLookup(address):
+	
 	print("I'm asking the XRPL for wallet stats'")
 	var message = {}
 	message.method = "account_info"
 	var paramObject = {}
-	paramObject.account = "rJab1KS2TnBHHmRi2jdbaxc6geAgg3iCS3"
+	paramObject.account = address
 	paramObject.strict = true
 	paramObject.ledger_index = "current"
 	message.params = [paramObject]
-	##var headers = ["Content-Type: application/json"]
-	##var query = JSON.print(message)
 	print(message)
+	
 	var http = HTTPRequest.new()
 	get_tree().root.add_child(http)
-	
-	print("I got here")
-	##http.request("https://s1.ripple.com:51234/", headers, true, HTTPClient.METHOD_GET, query)
-	
-	var data_to_send = {"method": "account_nfts","params": [{"account": "rJab1KS2TnBHHmRi2jdbaxc6geAgg3iCS3","ledger_index": "validated"}]}
-	var query = JSON.print(data_to_send)
+	var body = JSON.print(message)
 	var headers = ["Content-Type: application/json"]
 
-	http.request("http://localhost:8080/mintAndOffer", headers, true, HTTPClient.METHOD_POST, query)
+	http.request("https://xrplcluster.com/", headers, true, HTTPClient.METHOD_POST, body)
+	var response = yield(http, "request_completed")
+	response = response[3].get_string_from_utf8()
+	return response
+	
+func accountNFTs(address):
+	print("I'm asking the XRPL for NFTs'")
+	var message = {}
+	message.method = "account_nfts"
+	var paramObject = {}
+	paramObject.account = address
+	paramObject.strict = true
+	paramObject.ledger_index = "current"
+	message.params = [paramObject]
+	print(message)
+	
+	var http = HTTPRequest.new()
+	get_tree().root.add_child(http)
+	var body = JSON.print(message)
+	var headers = ["Content-Type: application/json"]
+
+	http.request("https://xrplcluster.com/", headers, true, HTTPClient.METHOD_POST, body)
+	var response = yield(http, "request_completed")
+	response = response[3].get_string_from_utf8()
+	return response
+	
+func accountTrustlines(address):
+	print("I'm asking the XRPL for Trustlines'")
+	var message = {}
+	message.method = "account_lines"
+	var paramObject = {}
+	paramObject.account = address
+	paramObject.strict = true
+	paramObject.ledger_index = "current"
+	message.params = [paramObject]
+	print(message)
+	
+	var http = HTTPRequest.new()
+	get_tree().root.add_child(http)
+	var body = JSON.print(message)
+	var headers = ["Content-Type: application/json"]
+
+	http.request("https://xrplcluster.com/", headers, true, HTTPClient.METHOD_POST, body)
+	var response = yield(http, "request_completed")
+	response = response[3].get_string_from_utf8()
+	return response
+	
+func accountXUMMStatus(address):
+	print("I'm asking the XRPL for XUMM status'")
+	
+	var http = HTTPRequest.new()
+	get_tree().root.add_child(http)
+	var headers = ["Content-Type: application/json"]
+
+	http.request("https://xumm.app/api/v1/platform/account-meta/" + address, headers, true, HTTPClient.METHOD_GET)
+	var response = yield(http, "request_completed")
+	response = response[3].get_string_from_utf8()
+	return response
+
--- addons/xrpl4gd/main.gd
+++ addons/xrpl4gd/main.gd
@@ -1,12 +1,26 @@
 tool
 extends Node
 
+onready var address_input = get_node("Wallet Address LineEdit")
+
 func _ready():
 	$"Get Stats Button".connect("pressed", self, "_on_button_pressed")
 	
 func _on_button_pressed():
-	var address = "rpYdCHsTgvfcpSNkrhgKpUBJoGh8s8AJum"
 	
-	WalletStats.addressLookup()
-	##$"Funds HBox/Funds LineEdit".text = WalletStats.addressLookup()
-	print(address)
+	var wallet_info = yield(WalletStats.addressLookup(address_input.get_text()), "completed")
+	var parsedMessage = JSON.parse(wallet_info)
+	print(parsedMessage)
+	$"Funds HBox"/"Funds LineEdit".text = String(int(parsedMessage.result.result.account_data.Balance) / 1000000) + " XRP"
+	
+	var account_nfts = yield(WalletStats.accountNFTs(address_input.get_text()), "completed")
+	parsedMessage = JSON.parse(account_nfts)
+	print(account_nfts)
+	
+	var account_trustlines = yield(WalletStats.accountTrustlines(address_input.get_text()), "completed")
+	parsedMessage = JSON.parse(account_trustlines)
+	print(account_trustlines)
+	
+	var account_xumm_status = yield(WalletStats.accountXUMMStatus(address_input.get_text()), "completed")
+	parsedMessage = JSON.parse(account_xumm_status)
+	print(account_xumm_status)
